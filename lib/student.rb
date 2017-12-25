abe91a2c12e33255f5976a148a5672feb5569106
Yomaira Escano <yjescano@gmail.com>
2017-12-25 05:28:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -102,6 +102,14 @@ class Student
   end
 
   def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql)
   end
 
 end
