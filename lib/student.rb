7aef120738134c05e6750f7d5ee9c7eca7897421
Yomaira Escano <yjescano@gmail.com>
2017-12-25 05:35:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,6 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
     new_student = self.new
     new_student.id = row[0]
     new_student.name = row[1]
@@ -11,8 +10,6 @@ class Student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
     sql = <<-SQL
       SELECT *
       FROM students
@@ -24,8 +21,6 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
     sql = <<-SQL
       SELECT *
       FROM students
