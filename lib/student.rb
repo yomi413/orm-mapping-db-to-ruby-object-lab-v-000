356b3a474e9a9c4ab4a16125a54f9bab046ebecb
Yomaira Escano <yjescano@gmail.com>
2017-12-25 04:38:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
     # remember each row should be a new instance of the Student class
     sql = <<-SQL
       SELECT *
-      FROM songs
+      FROM students
     SQL
 
     DB[:conn].execute(sql).map do |row|
